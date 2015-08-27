7bc172a75baed39de9d8f34bcc22086e2be30e51
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-08-27 12:14:42 -0400

    Update ember-cli to 1.13.8
    Also added author and repository to package.json

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,7 +1,7 @@
-import Ember from 'ember'
+import Ember from 'ember';
 import EmberValidations from 'ember-validations';
 
-const { Controller } = Ember
+const { Controller } = Ember;
 
 export default Controller.extend(EmberValidations, {
   validations: {
